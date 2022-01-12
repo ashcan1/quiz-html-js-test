446337763169c4922ad53bc0167bb9b3e78b3241
ashcan1 <ashki444@gmail.com>
2022-01-12 12:36:28 +0000

    end

---
+++ game.js
@@ -0,0 +1,113 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        choice1: '<js>',
+        choice2: '<script>',
+        choice3: '<javascript>',
+        choice4: 'scriping',
+        answer: 2,
+    },
+    {
+        question:
+            "Where is the correct place to insert a JavaScript?",
+        choice1: "the <body> section",
+        choice2: "the <head>",
+        choice3: "both",
+        choice4: "None of the above",
+        answer: 3,
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called xxx.js",
+        choice1: "<script href",
+        choice2: "script name",
+        choice3: "script src",
+      
+        answer: 3,
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+  
+        choice1: "function myFunction()",
+        choice2: "function:myFunction()",
+        choice3: "function = myFunction",
+        answer: 1,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
